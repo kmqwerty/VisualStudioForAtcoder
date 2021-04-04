33c84ad5a4f7fb0a187ef594e71712e56a1867e9
kmqwerty <40133734+kmqwerty@users.noreply.github.com>
2021-04-05 00:37:17 +0900

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+int main()
+{
+
+}
